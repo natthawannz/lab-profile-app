d6a17ddfd619575177cce15fcc47f69cbcbd1890
Natthawan Puangmalai <119285262+natthawannz@users.noreply.github.com>
2025-07-08 10:57:20 +0700

    .

---
+++ app/_layout.js
@@ -0,0 +1,36 @@
+import { Stack } from "expo-router";
+import ThemeToggle from "./components/ThemeToggle";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
+
+function StackLayout() {
+  const { color } = useTheme();
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: color.background,
+        },
+        headerTintColor: color.text,
+        headerTitleStyle: {
+          color: color.text,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{ title: "Profile", headerRight: () => <ThemeToggle /> }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{ title: "About us", headerRight: () => <ThemeToggle /> }}
+      />
+    </Stack>
+  );
+}
+export default function Layout() {
+  return (
+    <ThemeProvider>
+      <StackLayout />
+    </ThemeProvider>
+  );
+}
